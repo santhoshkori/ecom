2136cec2922d9918b56fbcfc19e4bcd27aa03c01
vamshi <vamshikori@gmail.com>
2023-12-04 10:51:33 +0000

    unable to update

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    console.log(cartList, product.id)
+    if (cartList.length === 0) {
+      this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+    } else {
+      const istrue = cartList.find(c => c.id === product.id)
+      if (istrue !== undefined) {
+        this.setState(prevState => ({
+          cartList: prevState.cartList.map(eachcartlist => {
+            if (eachcartlist.id === product.id) {
+              return {...eachcartlist, quantity: eachcartlist.quantity + 1}
+            }
+            return null
+          }),
+        }))
+        console.log('hy')
+      } else {
+        this.setState(prevState => ({
+          cartList: [...prevState.cartList, product],
+        }))
+      }
+      console.log(istrue)
+    }
+
+    //   TODO: Update the code here to implement addCartItem
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
